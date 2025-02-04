7c5bb98500554ee1d395a07e32c11d8f1925db75
arifditya30 <aryfadtya@gmail.com>
2025-02-04 17:23:19 +0700

    Components based

---
+++ resources/js/lib/utils.js
@@ -0,0 +1,22 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs) {
+    return twMerge(clsx(inputs));
+}
+
+export const STATUS = {
+    TODO: 'To Do',
+    INPROGRESS: 'In Progress',
+    ONREVIEW: 'On Review',
+    DONE: 'Done',
+    UNKNOWN: 'Unknown',
+};
+
+export const PRIORITY = {
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: 'High',
+    UNKNOWN: 'Unknown',
+    URGENT: 'Urgent',
+};
